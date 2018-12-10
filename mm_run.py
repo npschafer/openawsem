8daa367de45381c06649a673644740c6b7b0d270
luwei0917 <luwei0917@gmail.com>
2018-12-10 17:28:51 -0600

    update mm_run.py

--- mm_run.py
+++ mm_run.py
@@ -31,7 +31,14 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("protein", help="The name of the protein")
 parser.add_argument("--name", default="simulation", help="Name of the simulation")
+parser.add_argument("--to", default="./", help="location of movie file")
 parser.add_argument("-c", "--chain", type=str, default="-1")
+parser.add_argument("-t", "--thread", type=int, default=-1, help="default is using all that is available")
+parser.add_argument("--platform", type=str, default="OpenCL")
+parser.add_argument("-s", "--step", type=int, default=int(1e5), help="step size")
+parser.add_argument("--simulation_mode", type=int, default=0,
+                help="default 0: constant temperature,\
+                        1: temperature annealing")
 args = parser.parse_args()
 
 
@@ -43,11 +50,13 @@ with open('commandline_args.txt', 'a') as f:
     f.write('\n')
 
 
-simulation_platform = "CPU"  # OpenCL, CUDA, CPU, or Reference
-simulation_platform = "OpenCL"
+# simulation_platform = "CPU"  # OpenCL, CUDA, CPU, or Reference
+# simulation_platform = "OpenCL"
+simulation_platform = args.platform
 platform = Platform.getPlatformByName(simulation_platform)
 if simulation_platform == "CPU":
-    platform.setPropertyDefaultValue("Threads", "1")
+    if args.thread != -1:
+        platform.setPropertyDefaultValue("Threads", str(args.thread))
     print(f"{simulation_platform}: {platform.getPropertyDefaultValue('Threads')} threads")
 
 proteinName = pdb_id = args.protein
@@ -58,6 +67,11 @@ pdb = f"{pdb_id}.pdb"
 if chain == "-1":
     chain = getAllChains("crystal_structure.pdb")
     print("Chains to simulate: ", chain)
+
+if args.to != "./":
+    os.system(f"mkdir -p {args.to}")
+    # os.system(f"cp {pdb} {args.to}/{pdb}")
+    # pdb = os.path.join(args.to, pdb)
 
 input_pdb_filename = f"{pdb_id}-openmmawsem.pdb"
 
@@ -100,14 +114,17 @@ simulation.context.setPositions(oa.pdb.positions) # set the initial positions of
 # simulation.context.setVelocitiesToTemperature(300*kelvin) # set the initial velocities of the atoms according to the desired starting temperature
 simulation.minimizeEnergy() # first, minimize the energy to a local minimum to reduce any large forces that might be present
 simulation.reporters.append(StateDataReporter(stdout, reporter_frequency, step=True, potentialEnergy=True, temperature=True)) # output energy and temperature during simulation
-simulation.reporters.append(PDBReporter("movie.pdb", reporter_frequency)) # output PDBs of simulated structures
+simulation.reporters.append(PDBReporter(os.path.join(args.to, "movie.pdb"), reporter_frequency))  # output PDBs of simulated structures
 
 print("Simulation Starts")
 start_time = time.time()
 
-for i in range(100):
-    integrator.setTemperature(3*(200-i)*kelvin)
-    simulation.step(10000)
+if args.mode == 0:
+    simulation.step(args.steps)
+elif args.mode == 1:
+    for i in range(100):
+        integrator.setTemperature(3*(200-i)*kelvin)
+        simulation.step(10000)
 
 #simulation.step(int(1e6))
 simulation.reporters.append(CheckpointReporter(checkpoint_file, checkpoint_reporter_frequency)) # save progress during the simulation

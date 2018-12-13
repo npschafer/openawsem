c4fe4d268cfc9b70c59d22b0aee754c8753cb590
luwei0917 <luwei0917@gmail.com>
2018-12-13 12:46:49 -0600

    add debye huckel term from Mingchen

--- openmmawsem.py
+++ openmmawsem.py
@@ -786,7 +786,7 @@ class OpenMMAWSEMSystem:
         ramaSS.setForceGroup(15)
         return ramaSS
 
-    def direct_term(self, k_direct=4.184):
+    def direct_term(self, k_direct=4.184*1.5):
         k_direct *= self.k_awsem
         # print(self.ca, self.cb)
         # print(self.bonds)
@@ -911,7 +911,7 @@ class OpenMMAWSEMSystem:
         return burial
 
 
-    def mediated_term(self, k_mediated=4.184):
+    def mediated_term(self, k_mediated=4.184*1.5):
         k_mediated *= self.k_awsem
         # print(self.nres)  # print 181 for 2xov
         # print(self.resi)  # print the rsidues index for each atom
@@ -1866,6 +1866,7 @@ class OpenMMAWSEMSystem:
         #beta_2.setForceGroup(24)
         beta_3.setForceGroup(25)
         return beta_3
+
     def pap_term(self):
         print("pap term ON");
         nres, ca = self.nres, self.ca
@@ -2026,7 +2027,38 @@ class OpenMMAWSEMSystem:
         er.setForceGroup(21)
         return er
 
+    def debye_huckel_term(self, k_dh = 4.15*4.184):
+         print("Debye Huckel term is ON")
+         k_dh *= self.k_awsem*0.1;
+         k_screening = 1.0;
+         screening_length = 1.0; #(in the unit of nanometers)
 
+         dh = CustomBondForce("k_dh*charge_i*charge_j/r*exp(-k_screening*r/screening_length)")
+         dh.addGlobalParameter("k_dh", k_dh)
+         dh.addGlobalParameter("k_screening", k_screening)
+         dh.addGlobalParameter("screening_length", screening_length)
+         dh.addPerBondParameter("charge_i");
+         dh.addPerBondParameter("charge_j")
+         structure_interactions_dh = []
+         for i in range(self.nres):
+             for j in range(i+1,self.nres):
+                 charge_i = 0.0;
+                 charge_j = 0.0;
+                 if self.seq[i] == "R" or self.seq[i]=="K":
+                     charge_i = 1.0;
+                 if self.seq[i] == "D" or self.seq[i]=="E":
+                     charge_i = -1.0;
+                 if self.seq[j] == "R" or self.seq[j]=="K":
+                     charge_j = 1.0;
+                 if self.seq[j] == "D" or self.seq[j]=="E":
+                     charge_j = -1.0;
+                 if charge_i*charge_j!=0.0:
+                     structure_interactions_dh.append([self.cb[i], self.cb[j], [charge_i, charge_j]]);
+                     #print([self.seq[i], self.seq[j],self.cb[i], self.cb[j], [charge_i, charge_j]])
+         for structure_interaction_dh in structure_interactions_dh:
+             dh.addBond(*structure_interaction_dh)
+         dh.setForceGroup(27)
+         return dh
 
     def qbias_term(self, q0, reference_pdb_file, reference_chain_name, k_qbias=10000, qbias_min_seq_sep=3, qbias_max_seq_sep=np.inf, qbias_contact_threshold=0.8*nanometers):
         qbias = CustomCVForce("0.5*k_qbias*(q-q0)^2")

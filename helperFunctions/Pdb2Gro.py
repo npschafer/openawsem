be99e5e871f477cc6bcb0b19e598758277dfeb07
cabb99 <carlos.bueno@rice.edu>
2019-04-04 16:16:07 -0500

    Changes the format slightly to PEP8

--- helperFunctions/Pdb2Gro.py
+++ helperFunctions/Pdb2Gro.py
@@ -10,6 +10,7 @@
 
 import sys
 
+
 class Atom:
     atom_no = 0
     atom_name = ""
@@ -43,35 +44,36 @@ class Atom:
         print(self.atom_no, self.atom_name, self.res_no, self.res_name, self.x, self.y, self.z, self.desc)
 
     def write_(self, f):
-    	f.write( ("     "+str(self.res_no))[-5:] )
-    	f.write( ("     "+self.res_name)[-5:] )
-    	f.write( " " + (self.atom_name+"    ")[:4] )
-    	f.write( ("     "+str(self.atom_no))[-5:] )
-    	f.write( ("        "+str(round(self.x/10,3)))[-8:] )
-    	f.write( ("        "+str(round(self.y/10,3)))[-8:] )
-    	f.write( ("        "+str(round(self.z/10,3)))[-8:] )
-    	f.write("\n")
+        f.write(("     " + str(self.res_no))[-5:])
+        f.write(("     " + self.res_name)[-5:])
+        f.write(" " + (self.atom_name + "    ")[:4])
+        f.write(("     " + str(self.atom_no))[-5:])
+        f.write(("        " + str(round(self.x / 10, 3)))[-8:])
+        f.write(("        " + str(round(self.y / 10, 3)))[-8:])
+        f.write(("        " + str(round(self.z / 10, 3)))[-8:])
+        f.write("\n")
 
-if len(sys.argv)!=4 and len(sys.argv)!=3:
+
+if len(sys.argv) != 4 and len(sys.argv) != 3:
     print("\n> Pdb2Gro.py PDB_Id Output_file [Chain]\n")
     exit()
 
 from Bio.PDB.PDBParser import PDBParser
 
-p = PDBParser(PERMISSIVE=1,QUIET=True)
+p = PDBParser(PERMISSIVE=1, QUIET=True)
 
 pdb_file = sys.argv[1]
 pdb_id = pdb_file
-if pdb_file[-4:].lower()!=".pdb":
-	pdb_file = pdb_file + ".pdb"
-if pdb_id[-4:].lower()==".pdb":
-	pdb_id = pdb_id[:-5]
+if pdb_file[-4:].lower() != ".pdb":
+    pdb_file = pdb_file + ".pdb"
+if pdb_id[-4:].lower() == ".pdb":
+    pdb_id = pdb_id[:-5]
 
 output = sys.argv[2]
 
 chain_name = ""
-if len(sys.argv)>=4:
-	chain_name = sys.argv[3]
+if len(sys.argv) >= 4:
+    chain_name = sys.argv[3]
 
 s = p.get_structure(pdb_id, pdb_file)
 chains = s[0].get_list()
@@ -79,27 +81,27 @@ chains = s[0].get_list()
 atoms = []
 
 for chain in chains:
-	if chain_name=="" or chain.get_id()==chain_name:
-		ires = 0
-		iatom = 0
-		res_name = ""
-		for res in chain:
-			is_regular_res = res.has_id('N') and res.has_id('CA') and res.has_id('C')
-			res_id = res.get_id()[0]
-			if (res_id ==' ' or res_id =='H_MSE' or res_id =='H_M3L' or res_id =='H_CAS') and is_regular_res:
-				ires = ires + 1
-				res_name = res.get_resname()
-				residue_no = res.get_id()[1]
-				for atom in res:
-					iatom = iatom + 1
-					atom_name = atom.get_name()
-					xyz = atom.get_coord()
-					atoms.append( Atom(iatom, atom_name, residue_no, res_name, xyz) )
+    if chain_name == "" or chain.get_id() == chain_name:
+        ires = 0
+        iatom = 0
+        res_name = ""
+        for res in chain:
+            is_regular_res = res.has_id('N') and res.has_id('CA') and res.has_id('C')
+            res_id = res.get_id()[0]
+            if (res_id == ' ' or res_id == 'H_MSE' or res_id == 'H_M3L' or res_id == 'H_CAS') and is_regular_res:
+                ires = ires + 1
+                res_name = res.get_resname()
+                residue_no = res.get_id()[1]
+                for atom in res:
+                    iatom = iatom + 1
+                    atom_name = atom.get_name()
+                    xyz = atom.get_coord()
+                    atoms.append(Atom(iatom, atom_name, residue_no, res_name, xyz))
 
 out = open(output, 'w')
 out.write(" Structure-Based gro file\n")
-out.write( ("            "+str(len(atoms)))[-12:] )
+out.write(("            " + str(len(atoms)))[-12:])
 out.write("\n")
 for iatom in atoms:
-	iatom.write_(out)
+    iatom.write_(out)
 out.close()

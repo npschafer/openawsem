3c31e70ce3ac3017c3d0880fdd877b8814e15f5c
luwei0917 <luwei0917@gmail.com>
2020-04-25 13:40:18 -0500

    minor updates

--- functionTerms/basicTerms.py
+++ functionTerms/basicTerms.py
@@ -14,7 +14,7 @@ def con_term(oa, k_con=50208, bond_lengths=[.3816, .240, .276, .153], forceGroup
     con = HarmonicBondForce()
     for i in range(oa.nres):
         con.addBond(oa.ca[i], oa.o[i], bond_lengths[1], k_con)
-        if not oa.res_type[i] == "IGL":  # OpenAWSEM system doesn't have virtual HB atom for glycine, so con_term doesn't include C_alpha and HB interaction in LAMMPS
+        if not oa.res_type[i] == "IGL":  # OpenAWSEM system doesn't have CB for glycine, so the following bond is not exist for Glycine, but LAMMPS include this bond by using virtual HB as CB.
             con.addBond(oa.ca[i], oa.cb[i], bond_lengths[3], k_con)
         if i not in oa.chain_ends:
             con.addBond(oa.ca[i], oa.ca[i+1], bond_lengths[0], k_con)
@@ -23,7 +23,7 @@ def con_term(oa, k_con=50208, bond_lengths=[.3816, .240, .276, .153], forceGroup
     return con
 
 
-def chain_term(oa, k_chain=50208, bond_lengths=[0.2459108, 0.2519591, 0.2466597], forceGroup=20):
+def chain_term(oa, k_chain=50208, bond_k=[1, 1, 1], bond_lengths=[0.2459108, 0.2519591, 0.2466597], forceGroup=20):
     # add chain forces
     # 50208 = 60 * 2 * 4.184 * 100. kJ/nm^2, converted from default value in LAMMPS AWSEM
     # multiply interaction strength by overall scaling
@@ -31,11 +31,11 @@ def chain_term(oa, k_chain=50208, bond_lengths=[0.2459108, 0.2519591, 0.2466597]
     chain = HarmonicBondForce()
     for i in range(oa.nres):
         if i not in oa.chain_starts and not oa.res_type[i] == "IGL":
-            chain.addBond(oa.n[i], oa.cb[i], bond_lengths[0], k_chain)
+            chain.addBond(oa.n[i], oa.cb[i], bond_lengths[0], k_chain*bond_k[0])
         if i not in oa.chain_ends and not oa.res_type[i] == "IGL":
-            chain.addBond(oa.c[i], oa.cb[i], bond_lengths[1], k_chain)
+            chain.addBond(oa.c[i], oa.cb[i], bond_lengths[1], k_chain*bond_k[1])
         if i not in oa.chain_starts and i not in oa.chain_ends:
-            chain.addBond(oa.n[i], oa.c[i], bond_lengths[2], k_chain)
+            chain.addBond(oa.n[i], oa.c[i], bond_lengths[2], k_chain*bond_k[2])
     chain.setForceGroup(forceGroup)
     return chain
 

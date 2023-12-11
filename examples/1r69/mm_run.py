83af1b626ad94161c46f71338b3791ff333eb35a
Finley Clark <fc36@ws1808.ctbp.rice.edu>
2023-12-11 13:09:56 -0600

    commented out import imp statements, which were unnecessary and lead to issues with newer versions of python, since the module was renamed importlib

--- examples/1r69/mm_run.py
+++ examples/1r69/mm_run.py
@@ -7,7 +7,8 @@ from random import seed, randint
 import argparse
 import platform
 from datetime import datetime
-import imp
+#import imp
+# not needed and deprecated for importlib
 from time import sleep
 import fileinput
 

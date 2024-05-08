0758212108f6e20b23a7cd7fbde1434ff4dd8011
Carlos Bueno <carlos.bueno@rice.edu>
2024-05-08 17:50:29 -0500

    Returns k_qbias term to 100 kcal/mol

--- openawsem/functionTerms/biasTerms.py
+++ openawsem/functionTerms/biasTerms.py
@@ -154,7 +154,7 @@ def partial_q_value(oa, reference_pdb_file, min_seq_sep=3, a=0.1, startResidueIn
     qvalue.setForceGroup(forceGroup)
     return qvalue
 
-def qbias_term(oa,reference_pdb_file, q0, reference_chain_name="ALL", k_qbias=200*kilocalorie_per_mole, qbias_min_seq_sep=3, qbias_max_seq_sep=np.inf, qbias_contact_threshold=0.8*nanometers, forceGroup=4):
+def qbias_term(oa,reference_pdb_file, q0, reference_chain_name="ALL", k_qbias=100*kilocalorie_per_mole, qbias_min_seq_sep=3, qbias_max_seq_sep=np.inf, qbias_contact_threshold=0.8*nanometers, forceGroup=4):
     k_qbias = k_qbias.value_in_unit(kilojoule_per_mole)   # convert to kilojoule_per_mole, openMM default uses kilojoule_per_mole as energy.
     qbias = CustomCVForce(f"0.5*{k_qbias}*(q-{q0})^2")
     # qbias = CustomCVForce(f"0.5*{k_qbias}*(q-q0)^2")
